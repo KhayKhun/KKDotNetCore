fd44dbc77d0159fea988c6f543a6dc7444dd0c51
KhayKhun <saikhaykhunmong@gmail.com>
2024-02-26 02:36:42 +0630

    [Complete] Realtime chart using SignalR, ApexChart

---
+++ KKDotNetCore.RealtimeChartMvcApp/Hubs/RealtimeHub.cs
@@ -0,0 +1,26 @@
+﻿using KKDotNetCore.RealtimeChartMvcApp.Models;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+
+namespace KKDotNetCore.RealtimeChartMvcApp.Hubs
+{
+    public class RealtimeHub : Hub
+    {
+        public async Task ServerReceiveMessage()
+        {
+
+            Random random = new Random();
+
+            var model = new PieChartResourceModel()
+            {
+                Data = new PieChartModel()
+                {
+                    Labels = new List<string> { "A", "B", "C", "D" },
+                    Series = new List<int> { random.Next(1,50), random.Next(1, 50), random.Next(1, 50), random.Next(1, 50), }
+                }
+            };
+
+            await Clients.All.SendAsync("ClientReceiveMessage", JsonConvert.SerializeObject(model));
+        }
+    }
+}
---
+++ KKDotNetCore.RealtimeChartMvcApp/Models/PieChartResourceModel.cs
@@ -0,0 +1,12 @@
+﻿namespace KKDotNetCore.RealtimeChartMvcApp.Models
+{
+    public class PieChartResourceModel
+    {
+        public PieChartModel Data {  get; set; }
+    }
+    public class PieChartModel
+    {
+        public List<int> Series { get; set; }
+        public List<string> Labels { get; set; }
+    }
+}
