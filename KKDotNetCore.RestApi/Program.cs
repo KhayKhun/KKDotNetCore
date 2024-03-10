890b212b7b28cdba0c94ecaf36150d0e6ea94726
KhayKhun <saikhaykhunmong@gmail.com>
2024-03-10 15:00:51 +0630

    -

--- KKDotNetCore.RestApi/Program.cs
+++ KKDotNetCore.RestApi/Program.cs
@@ -26,11 +26,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 app.UseCors(BlazorOrigin);
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
